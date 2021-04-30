ae4aba535e64b51ccb96a804a9ccbd95fa99f5a3
OsKahl <oskarkahl@hotmail.com>
2021-04-30 15:02:58 +0200

    Klasen HAUS; FELDER; HAUS eingefügt

---
+++ KUGELN.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse KUGELN.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class KUGELN
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse KUGELN
+     */
+    public KUGELN()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
