03526268aa7e49f63caf69c2ce066f919d03f51c
Moritz-io <moritz.wigge@gmail.com>
2021-05-07 14:28:06 +0200

    Kugel Attribute KugelID und KugelSpielerName

--- KUGELN.java
+++ KUGELN.java
@@ -8,26 +8,19 @@
 public class KUGELN
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
+    int KugelID;
+    String KugelSpielerName;
 
     /**
      * Konstruktor für Objekte der Klasse KUGELN
      */
-    public KUGELN()
+    public KUGELN(String name, int id)
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        this.KugelID = id;
+		this.KugelSpielerName = name;
+        
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+  
+    
 }
