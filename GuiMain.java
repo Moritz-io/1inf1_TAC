ccca4bb399cf380bf8a97e2f7dace459760a3db8
OsKahl <oskarkahl@hotmail.com>
2021-07-09 15:07:50 +0200

    Kugeln

--- GuiMain.java
+++ GuiMain.java
@@ -24,7 +24,7 @@ public class GuiMain{
         frame.add(panel);
       
         
-        roundButton = new RoundButton("Kugel 1",75);
+        roundButton = new RoundButton("Kugel 1.1",75);
         roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Blau.png"));
         panel.add(roundButton);
         roundButton.setLocation(400,100);
@@ -32,10 +32,43 @@ public class GuiMain{
         roundButton.setVisible(true);
         roundButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                System.out.println("Clicked???");
+                System.out.println("Clicked 1.1???");
             }
         });
-        roundButton = new RoundButton("Kugel 2",75);
+        roundButton = new RoundButton("Kugel 1.2",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Blau.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(400,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 1.2???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 1.3",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Blau.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(400,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 1.3???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 1.4",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Blau.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(400,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 1.4???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 2.1",75);
         roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Rot.png"));
         panel.add(roundButton);
         roundButton.setLocation(40,100);
@@ -43,27 +76,132 @@ public class GuiMain{
         roundButton.setVisible(true);
         roundButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                System.out.println("Clicked 2???");
+                System.out.println("Clicked 2.1???");
             }
         });
-        roundButton = new RoundButton("Kugel 3",75);
+        roundButton = new RoundButton("Kugel 2.2",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Rot.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(40,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 2.2???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 2.3",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Rot.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(40,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 2.3???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 2.4",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Rot.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(40,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 2.4???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 3.1",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Gelb.png"));
         panel.add(roundButton);
         roundButton.setLocation(80,100);
         roundButton.setEnabled(true);
         roundButton.setVisible(true);
         roundButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                System.out.println("Clicked 3???");
+                System.out.println("Clicked 3.1???");
             }
         });
-        roundButton = new RoundButton("Kugel 4",75);
+        roundButton = new RoundButton("Kugel 3.2",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Gelb.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(80,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 3.2???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 3.3",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Gelb.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(80,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 3.3???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 3.4",75);
+        roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Gelb.png"));
+        panel.add(roundButton);
+        roundButton.setLocation(80,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 3.4???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 4.1",75);
+                roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Grün.png"));
+
         panel.add(roundButton);
         roundButton.setLocation(120,100);
         roundButton.setEnabled(true);
         roundButton.setVisible(true);
         roundButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                System.out.println("Clicked 4???");
+                System.out.println("Clicked 4.1???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 4.2",75);
+                roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Grün.png"));
+
+        panel.add(roundButton);
+        roundButton.setLocation(120,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 4.2???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 4.3",75);
+                roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Grün.png"));
+
+        panel.add(roundButton);
+        roundButton.setLocation(120,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 4.3???");
+            }
+        });
+        roundButton = new RoundButton("Kugel 4.4",75);
+                roundButton.setIcon(new ImageIcon("C:\\Users\\KahlOsk\\Documents\\Info Q11\\Tac_2\\Bilder\\Grün.png"));
+
+        panel.add(roundButton);
+        roundButton.setLocation(120,100);
+        roundButton.setEnabled(true);
+        roundButton.setVisible(true);
+        roundButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Clicked 4.4???");
             }
         });
     }
